f3e81a41e030ae46b71f917967ed677d795554e6
ggaiti <ggaiti@hotmail.com>
2023-10-03 23:54:09 -0400

    connected routes in front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import React from "react";
+import Layout from "./components/Layout";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
 function App() {
-  return <div>HELLO</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
